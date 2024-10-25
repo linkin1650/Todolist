c15b5379689e20e1951fbf27a332880b48f2f42f
linkin1650 <linkin1650@gmail.com>
2024-10-25 16:13:24 +0800

    feat: add state to store todo input

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -73,6 +73,15 @@ const TodoInput = ({ inputValue, onChange, onKeyDown, onAddTodo }) => {
       <StyledLabelIcon className="icon" htmlFor="add-todo-input" />
       <StyledInputContainer>
         <input id="add-todo-input" type="text" placeholder="新增工作" />
+        <input
+          id="add-todo-input"
+          type="text"
+          placeholder="新增工作"
+          value={inputValue}
+          onChange={(event) => {
+            onChange?.(event.target.value);
+          }}
+        />
       </StyledInputContainer>
       <StyledAddTodoActionContainer>
         <button className="btn-reset">新增</button>
