bc2eaa8480beaa0dd076edb451e8522cb8311f83
linkin1650 <linkin1650@gmail.com>
2024-10-25 16:23:31 +0800

    feat add change mode and save handler/key down event

--- src/components/TodoCollection.jsx
+++ src/components/TodoCollection.jsx
@@ -14,7 +14,9 @@ const TodoCollection = ({
           <TodoItem
             key={todo.id}
             todo={todo}
+            onSave={({ id, title }) => onSave?.({ id, title })}
             onToggleDone={(id) => onToggleDone?.(id)}
+            onChangeMode={({ id, isEdit }) => onChangeMode?.({ id, isEdit })}
           />
         );
       })}
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -80,6 +80,36 @@ const TodoPage = () => {
     });
   };
 
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          };
+        }
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+
+  const handleSave = ({ id, title }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            id,
+            title,
+            isEdit: false,
+          };
+        }
+        return todo;
+      });
+    });
+  };
+
   return (
     <div>
       TodoPage
@@ -90,7 +120,12 @@ const TodoPage = () => {
         onAddTodo={handleAddTodo}
         onKeyDown={handleKeyDown}
       />
-      <TodoCollection todos={todos} onToggleDone={handleToggleDone} />
+      <TodoCollection
+        todos={todos}
+        onSave={handleSave}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+      />
       <Footer />
     </div>
   );
