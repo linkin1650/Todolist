8102b5447633c573d756bb70b09102541c19dbc5
linkin1650 <linkin1650@gmail.com>
2024-10-29 17:31:17 +0800

    feat: login api function and show alert

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,47 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { login } from 'api/auth';
+import Swal from 'sweetalert2';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleClick = async () => {
+    if (username.length === 0) {
+      return;
+    }
+
+    if (password.length === 0) {
+      return;
+    }
+
+    const { success, authToken } = await login({
+      username,
+      password,
+    });
+
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+      Swal.fire({
+        title: '登入成功',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 1000,
+        position: 'top',
+      });
+      return;
+    }
+
+    Swal.fire({
+      title: '登入失敗',
+      icon: 'error',
+      showConfirmButton: false,
+      timer: 1000,
+      position: 'top',
+    });
+  };
 
   return (
     <AuthContainer>
@@ -38,7 +75,7 @@ const LoginPage = () => {
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
       <Link to="/signup">
         <AuthLinkText>註冊</AuthLinkText>
       </Link>
